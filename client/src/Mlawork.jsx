7be9ff89116a9feea991ad657f87b35e985d27b9
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2023-11-17 10:27:09 +0530

    20 evening

--- client/src/Mlawork.jsx
+++ client/src/Mlawork.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./ResponsiveComponent.css";
 import SanjayData from "./sanjaydata.jpeg";
+import voters from "./voter.jpg";
 import Sanjayworks from "./sanjayworks.jpeg";
-import sanjayvid from "./sanjayvid.mp4";
-
+import gangulavid from "./gangulavid.mp4";
+import bjpkarya from "./bjpkarya.mp4";
+import sanjayvid from "./sanjayvid";
+import roadshowvid from "./roadshowvid.mp4";
 const ResponsiveComponent = () => (
   <div className="responsive-container">
     <div className="video-container">
@@ -16,11 +19,44 @@ const ResponsiveComponent = () => (
         style={{ border: "none" }} // Use inline style to remove border
       ></iframe>
     </div>
+    <div className="video-container">
+      <iframe
+        title="Video"
+        width="100%"
+        height="100%"
+        src={gangulavid}
+        allowFullScreen
+        style={{ border: "none" }} // Use inline style to remove border
+      ></iframe>
+    </div>
+    <div className="video-container">
+      <iframe
+        title="Video"
+        width="100%"
+        height="100%"
+        src={bjpkarya}
+        allowFullScreen
+        style={{ border: "none" }} // Use inline style to remove border
+      ></iframe>
+    </div>
+    <div className="video-container">
+      <iframe
+        title="Video"
+        width="100%"
+        height="100%"
+        src={roadshowvid}
+        allowFullScreen
+        style={{ border: "none" }} // Use inline style to remove border
+      ></iframe>
+    </div>
     <div className="image-container">
       <img src={Sanjayworks} alt="Image 2" className="responsive-image" />
     </div>
     <div className="image-container">
       <img src={SanjayData} alt="Image 1" className="responsive-image" />
+    </div>
+    <div className="image-container">
+      <img src={voters} alt="Image 2" className="responsive-image" />
     </div>
   </div>
 );
